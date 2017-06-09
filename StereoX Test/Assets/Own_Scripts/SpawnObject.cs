5a41f99b6acb7e2151366429139cdb743f50891b
StereoXDHBWFN <stereox.dhbw.fn@gmail.com>
2017-06-09 12:10:22 +0200

    Spawn und Auswahl der OBJ
    Spawn funktioniert, Dalek import funktioniert nicht, weil zu viele
    vertices

--- StereoX Test/Assets/Own_Scripts/SpawnObject.cs
+++ StereoX Test/Assets/Own_Scripts/SpawnObject.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using VRTK.SecondaryControllerGrabActions;
+using VRTK.GrabAttachMechanics;
+using VRTK;
 
 public class SpawnObject : MonoBehaviour
 {
@@ -13,9 +16,9 @@ public class SpawnObject : MonoBehaviour
     [Tooltip("bla")]
     public bool dalek = false;
 
-    private string dalekpath = "C:/Users/Patrick/Desktop/dalekFull.obj";
+    private string dalekpath = "../Dateiconverter/OBJFiles/DalekFull/DalekFull.obj";
 
-    private string cubepath = "C:/Users/Patrick/Desktop/cube.obj";
+    private string cubepath = "../Dateiconverter/OBJFiles/Cube/cube.obj";
     public void SpawnObj()
     {
         Mesh holderMesh = new Mesh();
@@ -39,11 +42,19 @@ public class SpawnObject : MonoBehaviour
         obj.AddComponent<MeshFilter>();
         obj.GetComponent<MeshFilter>().mesh = holderMesh;
         obj.GetComponent<MeshRenderer>().material = newMaterialRef;
+        obj.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
         obj.transform.position = SpawnPoint.position;
         obj.transform.rotation = SpawnPoint.rotation;
         obj.AddComponent<Rigidbody>();
         obj.GetComponent<Rigidbody>().detectCollisions = true;
-
+        obj.AddComponent<BoxCollider>();
+        obj.AddComponent<VRTK_InteractableObject>();
+        obj.AddComponent<VRTK_FixedJointGrabAttach>();
+        obj.AddComponent<VRTK_AxisScaleGrabAction>();
+        obj.GetComponent<VRTK_AxisScaleGrabAction>().uniformScaling = true;
+        obj.GetComponent<VRTK_InteractableObject>().isGrabbable = true;
+        obj.GetComponent<VRTK_InteractableObject>().grabAttachMechanicScript = obj.GetComponent<VRTK_FixedJointGrabAttach>();
+        obj.GetComponent<VRTK_InteractableObject>().secondaryGrabActionScript = obj.GetComponent<VRTK_AxisScaleGrabAction>();
 
     }
 
@@ -53,10 +64,14 @@ public class SpawnObject : MonoBehaviour
         if (cube)
         {
             GameObject.Find("CubeToggle").GetComponent<Image>().color = Color.red;
+            dalek = false;
+            GameObject.Find("DalekToggle").GetComponent<Image>().color = Color.white;
         }
         else
         {
             GameObject.Find("CubeToggle").GetComponent<Image>().color = Color.white;
+            dalek = true;
+            GameObject.Find("DalekToggle").GetComponent<Image>().color = Color.red;
         }
     }
     public void toggleDalek()
@@ -65,10 +80,14 @@ public class SpawnObject : MonoBehaviour
         if (dalek)
         {
             GameObject.Find("DalekToggle").GetComponent<Image>().color = Color.red;
+            cube = false;
+            GameObject.Find("CubeToggle").GetComponent<Image>().color = Color.white;
         }
         else
         {
             GameObject.Find("DalekToggle").GetComponent<Image>().color = Color.white;
+            cube = true;
+            GameObject.Find("CubeToggle").GetComponent<Image>().color = Color.red;
         }
     }
 
